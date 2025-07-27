916d206f142ad433562ae3620da06116706065ee
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-07-27 00:12:54 -0400

    f   - reporting token usage

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
 )
 print(response.text)
-metadata = response.usage_metadata
-print('Prompt tokens: ' + str(metadata.prompt_token_count))
-print('Response tokens: ' + str(metadata.candidates_token_count))
+if response.usage_metadata:
+    metadata = response.usage_metadata
+    print('Prompt tokens: ' + str(metadata.prompt_token_count))
+    print('Response tokens: ' + str(metadata.candidates_token_count))
+else:
+    print('No usage metadata in response')
