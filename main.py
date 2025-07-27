a09a5b11584a3d0da2ee8ace59e0717215b551bd
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-07-27 04:58:58 -0400

    f   - changed message to list to hold context

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(response.text)
-if response.usage_metadata:
-    metadata = response.usage_metadata
-    print('Prompt tokens: ' + str(metadata.prompt_token_count))
-    print('Response tokens: ' + str(metadata.candidates_token_count))
-else:
-    print('No usage metadata in response')
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    api_key = os.environ.get('GEMINI_API_KEY')
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
